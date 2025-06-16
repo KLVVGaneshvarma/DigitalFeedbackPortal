15ebf1c1f0b2e8bd163cfd9f0e0ba8aa2edf987f
ganesh varma <karriganeshvarma1@gmail.com>
2025-06-16 15:47:54 +0530

    Second Changes

--- DigitalFeedbackPortal/obj/Debug/net8.0/DigitalFeedbackPortal.AssemblyInfo.cs
+++ DigitalFeedbackPortal/obj/Debug/net8.0/DigitalFeedbackPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalFeedbackPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2852d31851b54414e1bf02bc961332da3a3db4e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalFeedbackPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalFeedbackPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
